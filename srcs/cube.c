48d4b43fe3c25191a0f5f60dd3598b5bf31b168b
Remi FRISCCA <rfriscca@e1r11p7.42.fr>
2017-03-02 16:33:35 +0100

    norme sur cube.c

--- srcs/cube.c
+++ srcs/cube.c
@@ -19,43 +19,37 @@ void	cube_normale(t_vector *n, t_vector *o, t_obj *obj)
 	v.x = o->x - obj->pos.x;
 	v.y = o->y - obj->pos.y;
 	v.z = o->z - obj->pos.z;
+	n->x = 0;
+	n->y = 0;
+	n->z = 0;
 	if ((v.x > obj->cap2 - EPS && v.x < obj->cap2 + EPS) ||
 			(v.x > obj->cap1 - EPS && v.x < obj->cap1 + EPS))
-	{
 		n->x = 1;
-		n->y = 0;
-		n->z = 0;
-	}
-	if ((v.y > obj->cap2 - EPS && v.y < obj->cap2 + EPS) ||
+	else if ((v.y > obj->cap2 - EPS && v.y < obj->cap2 + EPS) ||
 			(v.y > obj->cap1 - EPS && v.y < obj->cap1 + EPS))
-	{
-		n->x = 0;
 		n->y = 1;
-		n->z = 0;
-	}
-	if ((v.z > obj->cap2 - EPS && v.z < obj->cap2 + EPS) ||
+	else if ((v.z > obj->cap2 - EPS && v.z < obj->cap2 + EPS) ||
 			(v.z > obj->cap1 - EPS && v.z < obj->cap1 + EPS))
-	{
-		n->x = 0;
-		n->y = 0;
 		n->z = 1;
-	}
 }
 
-void	trace_cube(t_obj *obj, t_vector o, t_vector v)
+void	swap_cap(t_obj *obj)
 {
-	float		t;
-	float		t1;
-	int			save;
-	t_equation	eq;
+	int		save;
 
-	sub_vector(&o, &obj->pos);
+	save = 0;
 	if (obj->cap1 > obj->cap2)
 	{
 		save = obj->cap1;
 		obj->cap1 = obj->cap2;
 		obj->cap2 = save;
 	}
+}
+
+void	trace_cube_extend(t_obj *obj, t_vector o, t_vector v, float t[2])
+{
+	sub_vector(&o, &obj->pos);
+	swap_cap(obj);
 	if (v.x >= 0)
 	{
 		obj->x1 = (obj->cap1 - o.x) / v.x;
@@ -68,39 +62,50 @@ void	trace_cube(t_obj *obj, t_vector o, t_vector v)
 	}
 	if (v.y >= 0)
 	{
-		t = (obj->cap1 - o.y) / v.y;
-		t1 = (obj->cap2 - o.y) / v.y;
+		t[0] = (obj->cap1 - o.y) / v.y;
+		t[1] = (obj->cap2 - o.y) / v.y;
 	}
 	else
 	{
-		t = (obj->cap2 - o.y) / v.y;
-		t1 = (obj->cap1 - o.y) / v.y;
+		t[0] = (obj->cap2 - o.y) / v.y;
+		t[1] = (obj->cap1 - o.y) / v.y;
 	}
-	if (obj->x1 > t1 || t > obj->x2)
+}
+
+void	trace_cube_extend_z(t_obj *obj, t_vector o, t_vector v, float t[2])
+{
+	if (t[0] > obj->x1)
+		obj->x1 = t[0];
+	if (t[1] < obj->x2)
+		obj->x2 = t[1];
+	if (v.z >= 0)
+	{
+		t[0] = (obj->cap1 - o.z) / v.z;
+		t[1] = (obj->cap2 - o.z) / v.z;
+	}
+	else
+	{
+		t[0] = (obj->cap2 - o.z) / v.z;
+		t[1] = (obj->cap1 - o.z) / v.z;
+	}
+	if (obj->x1 > t[1] || t[0] > obj->x2)
+		obj->dst = -1;
+}
+
+void	trace_cube(t_obj *obj, t_vector o, t_vector v)
+{
+	float		t[2];
+	t_equation	eq;
+
+	trace_cube_extend(obj, o, v, t);
+	if (obj->x1 > t[1] || t[0] > obj->x2)
 		obj->dst = -1;
 	else
-	{
-		if (t > obj->x1)
-			obj->x1 = t;
-		if (t1 < obj->x2)
-			obj->x2 = t1;
-		if (v.z >= 0)
-		{
-			t = (obj->cap1 - o.z) / v.z;
-			t1 = (obj->cap2 - o.z) / v.z;
-		}
-		else
-		{
-			t = (obj->cap2 - o.z) / v.z;
-			t1 = (obj->cap1 - o.z) / v.z;
-		}
-		if (obj->x1 > t1 || t > obj->x2)
-			obj->dst = -1;
-	}
-	if (t > obj->x1)
-		obj->x1 = t;
-	if (t1 < obj->x2)
-		obj->x2 = t1;
+		trace_cube_extend_z(obj, o, v, t);
+	if (t[0] > obj->x1)
+		obj->x1 = t[0];
+	if (t[1] < obj->x2)
+		obj->x2 = t[1];
 	eq.x1 = obj->x1;
 	eq.x2 = obj->x2;
 	eq.obj = obj;
@@ -114,12 +119,4 @@ void	trace_cube(t_obj *obj, t_vector o, t_vector v)
 	}
 	else
 		obj->dst = solve_equation(&eq);
-/*	obj->x1 = eq.x1;
-	obj->x2 = eq.x2;
-	if (obj->x1 > (0 + EPS) && obj->x1 < 1e8)
-		obj->dst = obj->x1;
-	else if (obj->x2 > (0 + EPS) && obj->x2 < 1e8)
-		obj->dst = obj->x2;
-	else
-		obj->dst = 1e8;*/
 }
