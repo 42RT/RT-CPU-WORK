25e4aa615b334fd0befe181562f6302bf9be1765
Remi FRISCCA <rfriscca@e2r3p15.42.fr>
2017-03-20 15:57:40 +0100

    added comment on calculation of cone normale, and norm ok on cone.c

--- srcs/cone.c
+++ srcs/cone.c
@@ -13,9 +13,16 @@
 #include <raytracer.h>
 #include <math.h>
 
-void		trace_cone(t_obj *obj, t_vector o, t_vector v)
+/*
+** Normale is calculated using the gradient of the surface
+** x^2 + y^2 - z^2 * tan^2(ang) = 0, ang = half angle of the cone
+** the gradient is 2x + 2y - 2z * tan^2(ang)
+** gradient at point M(x0, y0, z0) is M'(2 * x0, 2 * y0, -2 * z0 * tan^2(ang))
+** Here a = tan(ang)
+*/
+
+void		init_cone_normale(t_obj *obj, t_vector o, t_vector v)
 {
-	t_equation	eq;
 	t_vector	dir;
 	t_vector	point;
 	float		a;
@@ -23,6 +30,20 @@ void		trace_cone(t_obj *obj, t_vector o, t_vector v)
 	dir.x = 0;
 	dir.y = 0;
 	dir.z = 1;
+	point.x = v.x * obj->dst + o.x;
+	point.y = v.y * obj->dst + o.y;
+	point.z = v.z * obj->dst + o.z;
+	normalize(&point);
+	a = tan(vec_dot(&dir, &point));
+	obj->n.x = 2 * point.x;
+	obj->n.y = 2 * point.y;
+	obj->n.z = -2 * point.z * a * a;
+}
+
+void		trace_cone(t_obj *obj, t_vector o, t_vector v)
+{
+	t_equation	eq;
+
 	mod_vector(obj, &o, &v);
 	eq.a = pow(v.x, 2) + pow(v.y, 2) - pow((double)obj->size / 700.0, 2)
 											* pow(obj->mod, 2) * pow(v.z, 2);
@@ -35,14 +56,7 @@ void		trace_cone(t_obj *obj, t_vector o, t_vector v)
 	eq.o = o;
 	eq.v = v;
 	obj->dst = solve_equation(&eq);
-	point.x = v.x * obj->dst + o.x;
-	point.y = v.y * obj->dst + o.y;
-	point.z = v.z * obj->dst + o.z;
-	normalize(&point);
-	a = tan(vec_dot(&dir, &point));
-	obj->n.x = 2 * point.x;
-	obj->n.y = 2 * point.y;
-	obj->n.z = -2 * point.z * a * a;
+	init_cone_normale(obj, o, v);
 }
 
 void		cone_normale(t_vector *n, t_vector *o, t_obj *obj)
